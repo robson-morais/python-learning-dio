8afe4ee1ee1d8b1d723d5f8af4ff2b7c5cee2bb5
robson-morais <robson.starkp2@gmail.com>
2025-11-05 21:11:50 -0300

    classes abstratas e implementacao de metodos abstratos

---
+++ 10-POO/09-classes-abstratas/01-abc_base.py
@@ -0,0 +1,23 @@
+from abc import ABC, abstractmethod
+
+class Pessoa(ABC):
+
+    @abstractmethod
+    def falar(self):
+        pass
+    
+    @abstractmethod
+    def andar(self):
+        pass
+
+class Crianca(Pessoa):
+
+    def falar(self):
+        print("Falando...")    
+
+    def andar(self):
+        print("Andando...")
+
+c = Crianca()
+c.falar()
+c.andar()
