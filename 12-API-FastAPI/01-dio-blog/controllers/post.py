82d0baaa20a0b88e2e9a3916959b40372b96fa5c
robson-morais <robson.starkp2@gmail.com>
2025-11-10 21:28:28 -0300

    refatoração do código e padronização das rotas

---
+++ 12-API-FastAPI/01-dio-blog/controllers/post.py
@@ -0,0 +1,71 @@
+from datetime import datetime, UTC
+from typing import Annotated
+
+from fastapi import Response, Cookie, FastAPI, status, Header, APIRouter
+
+from schemas.post import PostIn
+from views.post import PostOut
+
+router = APIRouter(prefix='/posts')
+
+@router.get('/type/{framework}',response_model=PostOut)
+def read_posts(framework):
+    return {'posts': [{'title': f'criando uma aplicacao com {framework}', 'date': datetime.now(UTC)},
+                      {'title': f'Testando a nova versão de {framework}', 'date': datetime.now(UTC)}
+                      ]
+            }
+
+@router.get('/type2/{framework}') #-> com tipagem nos parametros:
+def read_posts2(framework: int):
+    return {'posts': [{'title': f'testando {framework} como tipo', 'date': datetime.now(UTC)},
+                      {'title': f'tentando {framework}', 'date': datetime.now(UTC)}
+                      ]
+            }
+
+# ROTAS E ENDPOINTS: QUERY PARAMETERS
+
+fake_db = [
+    {'title': "Criando uma aplicacao com Django", 'date': datetime.now(UTC)},
+    {'title': "Criando uma aplicacao com FastAPI", 'date': datetime.now(UTC)},
+    {'title': "Criando uma aplicacao com Flask", 'date': datetime.now(UTC)},
+    {'title': "Criando uma aplicacao com Starlett", 'date': datetime.now(UTC)}
+]
+
+@router.get("/fakedb")
+def read_posts3():
+    return fake_db
+
+# skip -> quantos item pular, limit -> total de item a serem retornados a partir do skip
+@router.get("/fakedb2")
+def read_posts4(skip: int = 0, limit: int = len(fake_db)):
+    return fake_db[skip : skip + limit]
+
+fake_db2 = [
+    {'title': "Criando uma aplicacao com Django", 'date': datetime.now(UTC), 'published':True},
+    {'title': "Criando uma aplicacao com FastAPI", 'date': datetime.now(UTC), 'published':True},
+    {'title': "Criando uma aplicacao com Flask", 'date': datetime.now(UTC), 'published':False},
+    {'title': "Criando uma aplicacao com Starlett", 'date': datetime.now(UTC), 'published':True}
+]
+
+@router.get('/fakedb3') #Adicionando a condicao booleana no parametro + lidando com Cookies
+def read_posts5(response: Response, published: bool, limit: int, skip: int = 0, ads_id: Annotated[str | None, Cookie()] = None, user_agent: Annotated[str | None, Header()] = None, response_model=list[PostOut]):
+
+    response.set_cookie(key="user", value="rob.stark@dcx.com")
+    print(f"Cookie: {ads_id}")
+
+    print(f"Browser: {user_agent}")
+    posts = []
+    for post in fake_db2:
+        if len(posts) == limit:
+            break
+        if post['published'] is published:
+            posts.append(post)
+    return posts
+
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=PostOut)
+def create_post(post: PostIn):
+    fake_db2.append(post.model_dump())
+    return post
+
---
+++ 12-API-FastAPI/01-dio-blog/views/post.py
@@ -0,0 +1,6 @@
+from pydantic import BaseModel
+from datetime import datetime, UTC
+
+class PostOut(BaseModel):
+    title: str
+    author: str
